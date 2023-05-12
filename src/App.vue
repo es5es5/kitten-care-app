a50dbb3decfbe0601fd63bb17df8ff3ea1486d65
es5es5 <es5e5s@nate.com>
2023-05-12 16:54:54 +0900

    Ken

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,69 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <h2>basic</h2>
+    <Ken ref="ken1" :cssStyle="customStyle"/>
+    <button @click="play()">Play</button>
+    <button @click="pause()">Pause</button>
+
+    <h2>sprite-change</h2>
+    <Ken ref="ken2" :cssStyle="customStyle"/>
+    <button @click="kick()">Kick</button>
+
+    <h2>sprite+translate</h2>
+    <Ken ref="ken3" :cssStyle="customStyle"/>
+    <button @click="leftMove">◀</button>
+    <button @click="rightMove">►</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Ken from '@/components/sprite/ken/ken.vue';
+
+export default {
+  name: "App",
+  components: {
+    Ken
+  },
+  data() {
+    return {
+      customStyle: {
+         width: "70px",
+         height: "80px",
+         backgroundImage: "url(https://uploads.codesandbox.io/uploads/user/50ba5672-ab19-466c-a1a4-38e62b5eb553/S5rk-ken.png)"
+      },
+    }
+  },
+  methods: {
+    play() {
+      this.$refs.ken1.spriteRestart(() => {
+        this.$refs.ken1.basicGesture();
+      });
+    },
+    pause() {
+      this.$refs.ken1.spritePause();
+    },
+    kick() {
+      this.$refs.ken2.kickGesture();
+    },
+    leftMove() {
+      this.$refs.ken3.leftMove();
+    },
+    rightMove() {
+      this.$refs.ken3.rightMove();
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+h2 {
+  text-align: left;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -5,4 +7,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: process.env.ELECTRON=="true" ? './' : ".",
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
