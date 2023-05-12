fa79c4c762f7e59b2a015675c4ba435d0bc099ad
es5es5 <es5e5s@nate.com>
2023-05-12 16:58:58 +0900

    Prettier

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,64 @@
 <template>
   <div id="app">
     <h2>basic</h2>
-    <Ken ref="ken1" :cssStyle="customStyle"/>
+    <Ken ref="ken1" :cssStyle="customStyle" />
     <button @click="play()">Play</button>
     <button @click="pause()">Pause</button>
 
     <h2>sprite-change</h2>
-    <Ken ref="ken2" :cssStyle="customStyle"/>
+    <Ken ref="ken2" :cssStyle="customStyle" />
     <button @click="kick()">Kick</button>
 
     <h2>sprite+translate</h2>
-    <Ken ref="ken3" :cssStyle="customStyle"/>
+    <Ken ref="ken3" :cssStyle="customStyle" />
     <button @click="leftMove">◀</button>
     <button @click="rightMove">►</button>
   </div>
 </template>
 
 <script>
-import Ken from '@/components/sprite/ken/Ken.vue';
+import Ken from '@/components/sprite/ken/Ken.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    Ken
+    Ken,
   },
   data() {
     return {
       customStyle: {
-         width: "70px",
-         height: "80px",
-         backgroundImage: "url(https://uploads.codesandbox.io/uploads/user/50ba5672-ab19-466c-a1a4-38e62b5eb553/S5rk-ken.png)"
+        width: '70px',
+        height: '80px',
+        backgroundImage:
+          'url(https://uploads.codesandbox.io/uploads/user/50ba5672-ab19-466c-a1a4-38e62b5eb553/S5rk-ken.png)',
       },
     }
   },
   methods: {
     play() {
       this.$refs.ken1.spriteRestart(() => {
-        this.$refs.ken1.basicGesture();
-      });
+        this.$refs.ken1.basicGesture()
+      })
     },
     pause() {
-      this.$refs.ken1.spritePause();
+      this.$refs.ken1.spritePause()
     },
     kick() {
-      this.$refs.ken2.kickGesture();
+      this.$refs.ken2.kickGesture()
     },
     leftMove() {
-      this.$refs.ken3.leftMove();
+      this.$refs.ken3.leftMove()
     },
     rightMove() {
-      this.$refs.ken3.rightMove();
-    }
-  }
-};
+      this.$refs.ken3.rightMove()
+    },
+  },
+}
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/assets/mixins.js
+++ src/assets/mixins.js
@@ -1,4 +1,4 @@
-import Velocity from "velocity-animate";
+import Velocity from 'velocity-animate'
 
 export const Sprite = {
   data() {
@@ -13,38 +13,38 @@ export const Sprite = {
         duration: 0,
         easing: 0,
         loop: false,
-        queue: ""
+        queue: '',
       },
       currentScene: {
         x: 0,
-        y: 0
+        y: 0,
       },
-      save: true
-    };
+      save: true,
+    }
   },
   mounted() {
-    this.spriteInit(this.$el);
+    this.spriteInit(this.$el)
   },
   computed: {
     spriteWidth() {
-      return this.spriteStyle.width.slice(0, -2);
+      return this.spriteStyle.width.slice(0, -2)
     },
     spriteHeight() {
-      return this.spriteStyle.height.slice(0, -2);
-    }
+      return this.spriteStyle.height.slice(0, -2)
+    },
   },
   methods: {
     spriteInit(el) {
       // Init, config
-      this.spriteElement = el;
-      this.spriteStyle = window.getComputedStyle(this.spriteElement, null);
+      this.spriteElement = el
+      this.spriteStyle = window.getComputedStyle(this.spriteElement, null)
     },
     spritePlay(cb) {
       Velocity(
         this.spriteElement,
         {
           backgroundPositionX: [this.spriteCss.ex, this.spriteCss.sx],
-          backgroundPositionY: [this.spriteCss.ey, this.spriteCss.sy]
+          backgroundPositionY: [this.spriteCss.ey, this.spriteCss.sy],
         },
         {
           easing: [this.spriteCss.easing],
@@ -53,83 +53,81 @@ export const Sprite = {
           queue: this.spriteCss.queue,
           complete: () => {
             if (this.save) {
-              this.updateScene();
+              this.updateScene()
             }
-            if (typeof cb === "function") {
-              cb();
+            if (typeof cb === 'function') {
+              cb()
             }
-          }
-        }
-      );
+          },
+        },
+      )
     },
     spritePause(cb) {
       if (this.save) {
-        this.updateScene();
+        this.updateScene()
       }
-      Velocity(this.spriteElement, "stop");
+      Velocity(this.spriteElement, 'stop')
     },
     spriteRestart(cb) {
       // easing and duration depend on coordinates, so they need synchronization.
-      let originEasing = Math.abs(this.spriteCss.ex / this.spriteWidth);
+      let originEasing = Math.abs(this.spriteCss.ex / this.spriteWidth)
       this.spriteCss.easing = Math.abs(
-        (this.spriteCss.ex - this.spriteCss.sx) / this.spriteWidth
-      );
+        (this.spriteCss.ex - this.spriteCss.sx) / this.spriteWidth,
+      )
       this.spriteCss.duration =
-        this.spriteCss.easing * (this.spriteCss.duration / originEasing);
+        this.spriteCss.easing * (this.spriteCss.duration / originEasing)
 
       if (this.spriteCss.loop) {
-        this.spriteCss.loop = false;
+        this.spriteCss.loop = false
         this.spritePlay(() => {
-          cb();
-        });
+          cb()
+        })
       } else {
-        this.spritePlay();
+        this.spritePlay()
       }
     },
     updateScene() {
-      const bp = this.spriteElement.style.backgroundPosition
-        .trim()
-        .split(/\s+/);
+      const bp = this.spriteElement.style.backgroundPosition.trim().split(/\s+/)
       const pos = {
         left: bp[0].slice(0, -2),
-        top: bp[1].slice(0, -2)
-      };
-      this.spriteCss.sx = pos.left;
-      this.spriteCss.sy = pos.top;
+        top: bp[1].slice(0, -2),
+      }
+      this.spriteCss.sx = pos.left
+      this.spriteCss.sy = pos.top
     },
     moveToX(sx, ex) {
       // start point change
       if (!isNaN(sx)) {
-        this.spriteCss.sx = sx;
+        this.spriteCss.sx = sx
       }
       if (!isNaN(ex)) {
-        this.spriteCss.ex = ex;
+        this.spriteCss.ex = ex
       }
     },
     moveToY(sy, ey) {
       if (sy) {
-        this.spriteCss.sy = sy;
+        this.spriteCss.sy = sy
       }
       if (ey) {
-        this.spriteCss.ey = ey;
+        this.spriteCss.ey = ey
       }
     },
     setLoop(v = true) {
-      this.spriteCss.loop = v;
+      this.spriteCss.loop = v
     },
     playTo(frame, duration, initFnc, callbackFnc) {
       // 1 dimension
-      this.spriteCss.ex = -this.spriteWidth * frame;
+      this.spriteCss.ex = -this.spriteWidth * frame
       this.spriteCss.easing = Math.abs(
-        (this.spriteCss.ex - this.spriteCss.sx) / this.spriteWidth
-      );
-      this.spriteCss.duration = duration;
+        (this.spriteCss.ex - this.spriteCss.sx) / this.spriteWidth,
+      )
+      this.spriteCss.duration = duration
 
-      if (typeof init === "function") {
-        initFnc();
+      if (typeof init === 'function') {
+        initFnc()
       }
 
-      this.spritePlay(callbackFnc);
-    }
-  }
-};
+      this.spritePlay(callbackFnc)
+    },
+  },
+}
